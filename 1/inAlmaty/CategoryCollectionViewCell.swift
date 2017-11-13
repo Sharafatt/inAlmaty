a51d77797d3232820a16e45f8ee1f6fc427d4345
Unknown <sharafatik@mail.ru>
2017-11-13 12:12:33 +0600

    [Initial commit] Base project commit

---
+++ 1/inAlmaty/CategoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionViewCell.swift
+//  inAlmaty
+//
+//  Created by Sharafat on 06.11.2017.
+//  Copyright © 2017 KBTU. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryName: UILabel!
+    
+}
